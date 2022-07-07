de3278e2a89406124aa596c29999d029dad22bff
yuriyfomin17 <fominyura2012@gmail.com>
2022-07-07 15:13:31 +0300

    commit

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
+
 @Component
 public class StudentService {
     private final StudentRepository studentRepository;
@@ -16,5 +18,12 @@ public class StudentService {
     @GetMapping
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()){
+            throw new IllegalStateException("email take");
+        }
+        studentRepository.save(student);
     }
 }
